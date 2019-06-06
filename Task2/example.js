69b6f8900b93f993fe672b29afa2c032b3348ffb
Piotr Andrzejewski <piotr.andrzejewski00@gmail.com>
2019-06-06 21:16:37 +0200

    everything works

--- Task2/example.js
+++ Task2/example.js
@@ -1,5 +1,72 @@
 const riddleSolver = board => {
-  return board
+  let removed = false
+  do {
+    removed = removeAdjacent(board)
+    dropIntegers(board)
+  } while(removed);
+  return board;
 }
 
-module.exports = riddleSolver
+const removeAdjacent = board => {
+  let removed = false
+  board.forEach(row => {
+    const {array, removedResponse} = removeAdjacentInArray(row)
+    if (removedResponse === true)
+      removed = true
+  })
+  for(let i = 0; i < board[0].length; i++) {
+    let column = board.map(row => row[i])
+    let {array, removedResponse}  = removeAdjacentInArray(column)
+    if (removedResponse === true)
+      removed = true
+    board.forEach((row, idx) => {
+      row[i] = array[idx]
+    })
+  }
+  return removed
+}
+
+const removeAdjacentInArray = array => {
+  let removed = false
+  let count = 0
+  let prevNumber = -1
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === prevNumber && array[i] !== 0) {
+      count++
+      if (count === 3) {
+        removed = true
+        array[i - 2] = 0
+        array[i - 1] = 0
+        array[i] = 0
+      } else if (count > 3) {
+        array[i] = 0
+      }
+    } else {
+      count = 1
+      prevNumber = array[i]
+    }
+  }
+  return {array: array, removedResponse: removed}
+}
+
+const dropIntegers = board => {
+  for(let i = 0; i < board[0].length; i++) {
+    const column = board.map(row => row[i])
+    column.sort((a, b) => {
+      if(a === 0)
+        return -1
+      if(b === 0)
+        return 1
+      else
+        return 0
+    })
+    board.forEach((row, idx) => {
+      row[i] = column[idx]
+    })
+  }
+}
+
+module.exports.removeAdjacentInArray = removeAdjacentInArray
+module.exports.removeAdjacent = removeAdjacent
+module.exports.dropIntegers = dropIntegers
+module.exports.riddleSolver = riddleSolver
--- Task2/tests/example.test.js
+++ Task2/tests/example.test.js
@@ -1,6 +1,71 @@
 const example = require('../example')
 
-test('basic', () => {
-  const result = example([])
-  expect(result).toMatchObject([])
+test('removing adjacent integers in array', () => {
+  const result = example.removeAdjacentInArray([1, 2, 5, 5, 5, 4, 0, 8, 8, 8, 8, 2])
+  expect(result).toMatchObject({array: [1, 2, 0, 0, 0, 4, 0, 0, 0, 0, 0, 2], removedResponse: true})
+})
+
+test('removing adjacent integers in board', () => {
+  const board = [
+    [7, 7, 7, 5, 1, 7],
+    [1, 8, 1, 1, 1, 4],
+    [3, 2, 3, 9, 7, 6],
+    [9, 9, 3, 3, 6, 2],
+    [1, 9, 3, 1, 8, 7],
+    [5, 9, 2, 2, 4, 8]
+  ];
+  const expected = [
+    [0, 0, 0, 5, 1, 7],
+    [1, 8, 0, 0, 0, 4],
+    [3, 2, 0, 9, 7, 6],
+    [9, 0, 0, 3, 6, 2],
+    [1, 0, 0, 1, 8, 7],
+    [5, 0, 2, 2, 4, 8]
+  ];
+  example.removeAdjacent(board)
+  expect(board).toMatchObject(expected)
+})
+
+test('dropping integers in board', () => {
+  const board = [
+    [0, 0, 0, 5, 1, 7],
+    [1, 8, 0, 0, 0, 4],
+    [3, 2, 0, 9, 7, 6],
+    [9, 0, 0, 3, 6, 2],
+    [1, 0, 0, 1, 8, 7],
+    [5, 0, 2, 2, 4, 8]
+  ];
+  const expected = [
+    [0, 0, 0, 0, 0, 7],
+    [1, 0, 0, 5, 1, 4],
+    [3, 0, 0, 9, 7, 6],
+    [9, 0, 0, 3, 6, 2],
+    [1, 8, 0, 1, 8, 7],
+    [5, 2, 2, 2, 4, 8]
+  ];
+  example.dropIntegers(board)
+  expect(board).toMatchObject(expected)
+})
+
+
+test('making whole task', () => {
+  const board = [
+    [7, 7, 7, 5, 1, 7],
+    [1, 8, 1, 1, 1, 4],
+    [3, 2, 3, 9, 7, 6],
+    [9, 9, 3, 3, 6, 2],
+    [1, 9, 3, 1, 8, 7],
+    [5, 9, 2, 2, 4, 8]
+  ];
+  const expected = [
+    [0, 0, 0, 0, 0, 7],
+    [1, 0, 0, 0, 1, 4],
+    [3, 0, 0, 5, 7, 6],
+    [9, 0, 0, 9, 6, 2],
+    [1, 0, 0, 3, 8, 7],
+    [5, 8, 0, 1, 4, 8]
+  ];
+  const result = example.riddleSolver(board)
+  expect(result).toMatchObject(expected)
+
 })
